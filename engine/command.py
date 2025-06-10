0f9115d467f8fc3ec3ab25c9b2d4bdb9429d9345
vedant jain <vj66546@gmail.com>
2025-06-10 09:12:36 +0530

    day 4 complete

---
+++ engine/command.py
@@ -0,0 +1,37 @@
+import pyttsx3
+import speech_recognition as sr
+
+def speak(text):
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 174)
+    print(voices)
+    engine.say(text)
+    engine.runAndWait()
+
+def takecommand():
+
+    r=sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said:{query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+
+text=takecommand()
+
+speak(text)
+
